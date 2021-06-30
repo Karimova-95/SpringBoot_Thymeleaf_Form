578c0a4a4f2fc471e6335da7677f4990f82982b4
Zilya Karimova <zilja081295@mail.ru>
2021-06-30 20:41:39 +0500

    create project

---
+++ src/main/java/com/example/springbootthymeleafform/SpringbootthymeleafformApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootthymeleafform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootthymeleafformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootthymeleafformApplication.class, args);
+    }
+
+}
